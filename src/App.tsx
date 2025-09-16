12e08814a7c6b00f0185ba6c824f6c4f591c55dc
Harshit Duggal <harshitduggal101@gmail.com>
2025-09-16 16:34:05 +0530

    Replaced Toaster from main to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import "@xyflow/react/dist/style.css";
 import { Home } from "./pages/Home";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
     <>
       <Home />
+      <Toaster position="top-right" />
     </>
   );
 }
